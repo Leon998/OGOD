1f4713427894162d15b2120d4983af2bb57c2803
XuShi <50541193+Leon998@users.noreply.github.com>
2022-07-08 16:54:01 +0800

    Create evaluation.sh

---
+++ script/evaluation.sh
@@ -0,0 +1,19 @@
+cell_phone='cell phone'
+sports_ball='sports ball'
+wine_glass='wine glass'
+#! python ../evaluation.py --step 1 --thres 0.8 --object apple
+! python ../evaluation.py --step 1 --thres 0.8 --object banana
+#! python ../evaluation.py --step 1 --thres 0.8 --object bottle
+#! python ../evaluation.py --step 1 --thres 0.8 --object bowl
+#! python ../evaluation.py --step 1 --thres 0.8 --object "$cell_phone"
+#! python ../evaluation.py --step 1 --thres 0.8 --object clock
+#! python ../evaluation.py --step 1 --thres 0.8 --object cup
+#! python ../evaluation.py --step 1 --thres 0.8 --object fork
+#! python ../evaluation.py --step 1 --thres 0.8 --object knife
+#! python ../evaluation.py --step 1 --thres 0.8 --object mouse
+#! python ../evaluation.py --step 1 --thres 0.8 --object orange
+! python ../evaluation.py --step 1 --thres 0.8 --object remote
+! python ../evaluation.py --step 1 --thres 0.8 --object scissors
+! python ../evaluation.py --step 1 --thres 0.8 --object spoon
+! python ../evaluation.py --step 1 --thres 0.8 --object "$sports_ball"
+#! python ../evaluation.py --step 1 --thres 0.8 --object "$wine_glass"
